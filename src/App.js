ca1706336788c0b6cf121d204ad2f6ca9cf2ff46
leova <leovkin2003@gmail.com>
2022-09-02 19:58:16 +0300

    Project done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import News from './news/pages/News';
+import New from './news/pages/New';
+import Navigation from './shared/components/navigation/Navigation';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [needUpdate, setNeedUpdate] = useState(false);
+    const needUpdateHandler = () => setNeedUpdate(!needUpdate);
+
+    return (
+        <div className="App">
+            <Router>
+                <Navigation needUpdateHandler={needUpdateHandler} />
+                <main className='main-container'>
+                    <Routes>
+                        <Route path='/' element={<News needUpdate={needUpdate}/>} />
+                        <Route path='/news/:newId' element={<New />} />
+                    </Routes>
+                </main>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
